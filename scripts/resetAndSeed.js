576988c1b49c10062dd706254dc7d3ab6fc41162
bobmino <bob.mino@gmail.com>
2026-05-06 01:31:21 +0100

    Update resetAndSeed.js

--- scripts/resetAndSeed.js
+++ scripts/resetAndSeed.js
@@ -1,4 +1,11 @@
-import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 import mongoose from 'mongoose';
 import connectDB from '../config/db.js';
 
